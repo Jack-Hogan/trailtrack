7e4aaf5e1ee207d9669938e8a12367506e901119
Jack Hogan <jack.hogan.06@hotmail.com>
2025-07-18 12:11:51 +0100

    Initialize-repo

---
+++ src/main/java/com/exploreireland/trailtrack/TrailtrackApplication.java
@@ -0,0 +1,13 @@
+package com.exploreireland.trailtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailtrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrailtrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exploreireland/trailtrack/TrailtrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exploreireland.trailtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrailtrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
